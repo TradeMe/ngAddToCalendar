888d20cdc712e07a757d76f3d1b52b61c7fd7b24
dtapp <davidtappenden.dev@gmail.com>
2019-01-30 10:33:35 +1300

    Updated packages for angular 7 to match FrEnd

--- dist/ng-add-to-calendar.module.ngfactory.js
+++ dist/ng-add-to-calendar.module.ngfactory.js
@@ -9,6 +9,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var i0 = require("@angular/core");
 var i1 = require("./ng-add-to-calendar.module");
 var i2 = require("./service/ng-add-to-calendar.service");
-var NgAddToCalendarModuleNgFactory = i0.ɵcmf(i1.NgAddToCalendarModule, [], function (_l) { return i0.ɵmod([i0.ɵmpd(512, i0.ComponentFactoryResolver, i0.ɵCodegenComponentFactoryResolver, [[8, []], [3, i0.ComponentFactoryResolver], i0.NgModuleRef]), i0.ɵmpd(4608, i2.NgAddToCalendarService, i2.NgAddToCalendarService, []), i0.ɵmpd(512, i1.NgAddToCalendarModule, i1.NgAddToCalendarModule, [])]); });
+var NgAddToCalendarModuleNgFactory = i0.ɵcmf(i1.NgAddToCalendarModule, [], function (_l) { return i0.ɵmod([i0.ɵmpd(512, i0.ComponentFactoryResolver, i0.ɵCodegenComponentFactoryResolver, [[8, []], [3, i0.ComponentFactoryResolver], i0.NgModuleRef]), i0.ɵmpd(4608, i2.NgAddToCalendarService, i2.NgAddToCalendarService, []), i0.ɵmpd(1073742336, i1.NgAddToCalendarModule, i1.NgAddToCalendarModule, [])]); });
 exports.NgAddToCalendarModuleNgFactory = NgAddToCalendarModuleNgFactory;
 //# sourceMappingURL=ng-add-to-calendar.module.ngfactory.js.map
