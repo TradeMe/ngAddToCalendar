fe41aeea08b99b7ae6ba0508cde9c63c97fff225
Michael Zoellner <michael.zoellner@mailbox.org>
2020-03-24 10:54:05 +1300

    removed unneeded peer dependencies
    new version is 3.2.0

--- src/ng-add-to-calendar.module.ts
+++ src/ng-add-to-calendar.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { NgAddToCalendarService } from './service/ng-add-to-calendar.service';
 
-import { CalendarTypeEnum } from './model/calendar-type.enum';
-import { ICalendarEvent } from './model/calendar-event.model';
-
 @NgModule({
   declarations: [],
   providers: [NgAddToCalendarService],
