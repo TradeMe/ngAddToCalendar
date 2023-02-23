fb7b7d443e0617e388a6357aab0acf2ed087ac24
tdashwood-trademe <tristan.dashwood@trademe.co.nz>
2023-02-23 15:39:25 +1300

    updated to use ng-package.json

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NgAddToCalendarModule = exports.NgAddToCalendarService = void 0;
-// Public classes.
-var ng_add_to_calendar_service_1 = require("./service/ng-add-to-calendar.service");
-Object.defineProperty(exports, "NgAddToCalendarService", { enumerable: true, get: function () { return ng_add_to_calendar_service_1.NgAddToCalendarService; } });
-var ng_add_to_calendar_module_1 = require("./ng-add-to-calendar.module");
-Object.defineProperty(exports, "NgAddToCalendarModule", { enumerable: true, get: function () { return ng_add_to_calendar_module_1.NgAddToCalendarModule; } });
-//# sourceMappingURL=index.js.map
